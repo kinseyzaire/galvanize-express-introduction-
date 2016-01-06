43670928b6ebf6a37fce6b979b72a4de630de9a7
Kinsey Ashby <kinseyzaire@gmail.com>
2016-01-05 20:12:24 -0700

    added some comments to 01 calc

--- 01-calculator/learnin-shit-01/routes/index.js
+++ 01-calculator/learnin-shit-01/routes/index.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express Calculator' });
 });
 
+// I tried to make it so that instead of add, sub, mult, or div
+// I used :maths in the URL to then be able to pull the math functions out
+// and call return c = maths() which accesses a separate calc func of the same name
+// it didn't work. is there a way to use a variable that is a String
+// as a trigger for a function with the same name?????
+
 /* GET addition answer. */
 router.get('/add/:a/:b', function(req, res, next) {
   var a = req.params.a*1, b = req.params.b*1, maths = req.params.maths;
