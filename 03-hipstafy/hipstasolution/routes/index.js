9c633008a0831d293d7456eb74b9b56278e252f6
Kinsey Ashby <kinseyzaire@gmail.com>
2016-01-05 22:19:51 -0700

    as close as this hipsta thing is getting tonight

---
+++ 03-hipstafy/hipstasolution/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hipstafy' });
+});
+/* hipsta-this-shit */
+var formInfo;
+router.post('/hipstasolution', function (req, res, next) {
+  req.body;
+  formInfo = req.body;
+  console.log(formInfo);
+});
+/* GET solution page. */
+router.get('/hipstasolution', function(req, res, next) {
+  res.render('answers', formInfo);
+});
+
+module.exports = router;
